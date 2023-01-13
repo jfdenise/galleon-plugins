32adad24b564e486389d2246e4f254b59a7dda32
JF Denise <jdenise@redhat.com>
2023-01-13 18:24:41 +0100

    Compute checksum for stream artifacts in generated manifest

--- maven-plugin/src/test/java/org/wildfly/galleon/maven/ChannelGenerationTestCase.java
+++ maven-plugin/src/test/java/org/wildfly/galleon/maven/ChannelGenerationTestCase.java
@@ -24,6 +24,12 @@ import static org.wildfly.galleon.plugin.ArtifactCoords.newGav;
 
 import java.io.IOException;
 import java.lang.reflect.Field;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.apache.maven.artifact.Artifact;
 import org.apache.maven.artifact.DefaultArtifact;
@@ -59,6 +65,16 @@ public class ChannelGenerationTestCase {
         Artifact wildflyEEGalleonArtifact = new DefaultArtifact( "org.wildfly",  "wildfly-ee-galleon-pack", "27.0.0.Final",  "provided", "zip", "", null);
         project.getArtifacts().add(wildflyEEGalleonArtifact);
 
+        Artifact cliArtifact = new DefaultArtifact( "org.wildfly.core",  "wildfly-cli", "2.0",  "compile", "jar", "", null);
+        project.getArtifacts().add(cliArtifact);
+        Path cliJar = Files.createTempFile("test-cli.jar", null);
+        cliJar.toFile().deleteOnExit();
+        cliArtifact.setFile(cliJar.toFile());
+
+        Artifact cliClientArtifact = new DefaultArtifact( "org.wildfly.core",  "wildfly-cli", "2.0",  "compile", "jar", "client", null);
+        cliClientArtifact.setFile(cliJar.toFile());
+        project.getArtifacts().add(cliClientArtifact);
+
         Artifact microprofileConfigAPIArtifact = new DefaultArtifact( "org.eclipse.microprofile.config",  "microprofile-config-api", "2.0",  "compile", "jar", "", null);
         project.getArtifacts().add(microprofileConfigAPIArtifact);
 
@@ -70,18 +86,25 @@ public class ChannelGenerationTestCase {
         f2.setAccessible(true);
         f2.set(mojo, Boolean.TRUE);
 
-        String yamlChannelManifest = mojo.createYAMLChannelManifest(fp);
+        Path f = Files.createTempFile("test-fp.zip", null);
+        f.toFile().deleteOnExit();
+        String yamlChannelManifest = mojo.createYAMLChannelManifest(fp, f.toFile());
         ChannelManifest channelManifest = ChannelManifestMapper.fromString(yamlChannelManifest);
-        assertEquals(2, channelManifest.getStreams().size());
+        assertEquals(3, channelManifest.getStreams().size());
         assertEquals(1, channelManifest.getManifestRequirements().size());
         assertChannelManifestContainsRequirements(channelManifest, "org.wildfly", "wildfly-ee-galleon-pack", "27.0.0.Final");
-        assertChannelManifestContainsStream(channelManifest, "org.wildfly", "wildfly-galleon-pack", "27.0.0.Final");
-        assertChannelManifestContainsStream(channelManifest, "org.eclipse.microprofile.config", "microprofile-config-api", "2.0");
+        Set<String> keys = new HashSet<>();
+        keys.addAll(Arrays.asList("zip"));
+        assertChannelManifestContainsStream(channelManifest, "org.wildfly", "wildfly-galleon-pack", "27.0.0.Final", keys);
+        assertChannelManifestContainsStream(channelManifest, "org.eclipse.microprofile.config", "microprofile-config-api", "2.0", Collections.emptySet());
+        Set<String> keys2 = new HashSet<>();
+        keys2.addAll(Arrays.asList("jar", "client/jar"));
+        assertChannelManifestContainsStream(channelManifest, "org.wildfly.core", "wildfly-cli", "2.0", keys2);
     }
 
-    private static void assertChannelManifestContainsStream(ChannelManifest channel, String groupId, String artifactId, String version) {
+    private static void assertChannelManifestContainsStream(ChannelManifest channel, String groupId, String artifactId, String version, Set<String> checksumKeys) {
         channel.getStreams().stream()
-                .filter(s -> groupId.equals(s.getGroupId()) && artifactId.equals(s.getArtifactId()) && version.equals(s.getVersion()))
+                .filter(s -> groupId.equals(s.getGroupId()) && artifactId.equals(s.getArtifactId()) && version.equals(s.getVersion()) && checksumKeys.equals(s.getsha256Checksum().keySet()))
                 .findFirst().orElseThrow();
     }
 
